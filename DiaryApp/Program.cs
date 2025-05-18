5ba8818f586584f6aa96e84cdcfecc22ae796765
Chirag <chiragj2019@gmail.com>
2025-05-18 20:06:25 +0530

    small Updates

--- DiaryApp/Program.cs
+++ DiaryApp/Program.cs
@@ -1,7 +1,14 @@
+using DiaryApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
@@ -14,16 +21,18 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.MapStaticAssets(); 
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=MyPage}/{id?}")
     .WithStaticAssets();
 
 
 app.Run();
+          
