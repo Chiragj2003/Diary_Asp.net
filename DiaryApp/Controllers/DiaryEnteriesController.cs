55baa0a1a2ca7015e5fc35d93026d31bf1ae380c
Chirag <chiragj2019@gmail.com>
2025-05-22 16:05:11 +0530

    complete

--- DiaryApp/Controllers/DiaryEnteriesController.cs
+++ DiaryApp/Controllers/DiaryEnteriesController.cs
@@ -1,6 +1,7 @@
 ﻿using DiaryApp.Data;
 using DiaryApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DiaryApp.Controllers
 {
@@ -36,6 +37,72 @@ namespace DiaryApp.Controllers
             }
 
             return View(obj); // return the view with the model if not valid
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound(); 
+            }
+
+            DiaryEntry? obj = _db.DiaryEntries.Find(id); 
+            if (obj == null) 
+            {
+                return NotFound(); 
+            }
+            return View(obj); 
+        }
+
+        [HttpPost]
+        public IActionResult Edit(DiaryEntry obj)
+        {
+            if (obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "Title too Short");
+            }
+            if (ModelState.IsValid) // check if model state is valid
+            {
+                _db.DiaryEntries.Update(obj); // update diary enteries in the database
+                _db.SaveChanges(); // save ch anges to database
+                return RedirectToAction("Index");
+            }
+
+            return View(obj); // return the view with the model if not valid
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            DiaryEntry? obj = _db.DiaryEntries.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var entry = _db.DiaryEntries.FirstOrDefault(e => e.Id == id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            _db.DiaryEntries
+                .Where(e => e.Id == id)
+                .ExecuteDelete(); // EF Core 7+
+
+            return RedirectToAction("Index");
         }
     }
 }
